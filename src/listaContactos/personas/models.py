b46ff64362e698ae7bd8c2d6a73ed5f10050edbd
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-06-05 19:00:04 -0500

    Cambiando el tipo de edad a IntegerField

--- src/listaContactos/personas/models.py
+++ src/listaContactos/personas/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Persona(models.Model):
     nombres   = models.TextField()
     apellidos = models.TextField()
-    edad      = models.TextField()
+    edad      = models.IntegerField()
