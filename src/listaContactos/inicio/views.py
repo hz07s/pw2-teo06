990bbcdaf17b5e496584f4c07afa457d40c28c95
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-06-06 19:05:04 -0500

    Probando templates, creando home.html y realizando sus configuraciones en settings.py

--- src/listaContactos/inicio/views.py
+++ src/listaContactos/inicio/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def myHomeView(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
-    return HttpResponse('<h1>Hola Mundo desde Django</h1>')
+    return render(request, "home.html", {})
 
 def anotherView(*args, **kwargs):
     return HttpResponse('<h1>Sólo es otra página</h1>')
