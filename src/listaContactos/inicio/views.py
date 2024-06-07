ca37908543531b0946a89faa6ecd003fb290848d
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-06-06 19:04:04 -0500

    Imprimiendo request.user, args y kwargs para ver su contenido

--- src/listaContactos/inicio/views.py
+++ src/listaContactos/inicio/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def myHomeView(*args, **kwargs):
+def myHomeView(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
     return HttpResponse('<h1>Hola Mundo desde Django</h1>')
+
+def anotherView(*args, **kwargs):
+    return HttpResponse('<h1>Sólo es otra página</h1>')
